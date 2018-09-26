e720148272062fff27a95b98891fa8cfa261a8ae
Shashank Iyer <shashanksundariyer@gmail.com>
2018-09-26 13:11:01 -0700

    Bug fixes

--- utilities/Query_1024.py
+++ utilities/Query_1024.py
@@ -1,9 +1,7 @@
 from clarifai.rest import ClarifaiApp
-import os, requests, sys
+from clarifai.rest import Image as ClImage
 from PIL import Image
-
-CLARIFAI_API_KEY = ''
-PATH = ''
+import requests, argparse
 
 def url_download_to_image(file, url):
     '''Downloads a file from the url and correctly converts it to an image.
@@ -16,55 +14,89 @@ def url_download_to_image(file, url):
         for block in r.iter_content(1024):
             handle.write(block)
 
-def download_images_obtain_1024_vector():
-    file = open('embeddings.txt', 'w')
-    counter = 0
-    for item in response:
-        counter+=1
-        print(str(counter) + ") URL: " + item.url + ", REGIONS: " + str(item.regions))
-        url_download_to_image('pic'+ str(counter)+'.jpg', item.url)
-        f = model.predict([item])
-        file.write(str(f['outputs'][0]['data']['embeddings'][0]['vector']) + '\n')
-        if counter == 4:
-            break
-
-    file.close()
-
-def obtain_1024_vector():
-    file = open(os.path.join(PATH,'embeddings.txt'))
-    counter = 0
-    for item in response:
-        counter+=1
-        print(str(counter) + ") URL: " + item.url + ", REGIONS: " + str(item.regions))
-        f = model.predict([item])
-        file.write(str(f['outputs'][0]['data']['embeddings'][0]['vector']) + '\n')
-
-    file.close()
-'''
-if len(sys.argv) == 3: 
-    CLARIFAI_API_KEY = sys.argv[1]
-    PATH = sys.argv[2]
-elif len(sys.argv) > 3:
-    print('Too many arguments')
-    exit(1)
-else:
-    if len(CLARIFAI_API_KEY)==0:
-        print('Please provide a key')
+def check_if_image(img_path):
+    '''Checks if the file is a valid image.
+       
+       :param img- absolute path to an image file
+    '''
+    try:
+        Image.open(img_path)
+    except IOError:
+        print(img_path, "is not a valid image")
         exit(1)
-    elif len(PATH)==0:
-        print('Please provide a PATH to the folder containing the query image')
-        exit(1)
-'''
-#def query_CLARIFAI():
-app = ClarifaiApp(api_key= CLARIFAI_API_KEY)
-img_num = 0
 
-response = app.inputs.get_all()
+def download_images(counter, url):
+    '''Downloads images from a custom Clarifai database and
+       obtains embeddings for each of them
+    '''
 
-counter = 0
-img_num+=1
-model = app.models.get('general-v1.3', model_type='embed')
-func_dict = {'y':download_images_obtain_1024_vector, 'n':obtain_1024_vector}
-cmd = input('Would you want to download the database images? (y/n)')    
+    url_download_to_image('pic'+ str(counter)+'.jpg', url)
 
-func_dict[cmd]()
+def skip_download_images(counter, url):
+    pass
+
+def _1024_embeddings(img, file, model):
+    '''Obtains embeddings and writes them to a file handle
+    '''
+    f = model.predict([img])
+    file.write(str(f['outputs'][0]['data']['embeddings'][0]['vector']) + '\n')
+
+def query_CLARIFAI(CLARIFAI_API_KEY, cmd='2', query_image = None):
+    '''Queries Clarifai's API to obtain embeddings of images
+    '''
+    app = ClarifaiApp(api_key= CLARIFAI_API_KEY)
+    img_num = 0
+
+    counter = 0
+    img_num+=1
+    model = app.models.get('general-v1.3', model_type='embed')
+    func_dict = {'1':download_images, '2': skip_download_images}
+    
+    if __name__ == '__main__':
+        print('Choose from the following options:')
+        print('Enter "1" to download images from your custom database and obtain embeddings')
+        print('Enter "2" to only obtain embeddings of images in your database')
+        print('Enter "3" to obtain an embedding of a new query images')
+        cmd = input('Choose either 1, 2 or 3: ')
+
+        if cmd == '3':
+            query_image = input('Enter the name of your query image')    
+
+    ret = 0
+    with open('embeddings.txt', 'w') as file:
+
+        if cmd == '1' or cmd == '2':
+            response = app.inputs.get_all()
+            counter = 0
+            for item in response:
+                counter+=1
+                print(str(counter) + ") URL: " + item.url)
+                func_dict[cmd](counter, item.url)
+                _1024_embeddings(item, file, model)
+                if counter == 4:
+                    break
+
+        elif query_image!=None:
+            check_if_image(query_image)
+            _1024_embeddings(ClImage(filename= query_image), file, model)
+        
+        ret = file
+
+    return ret
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Obtain the representation of an image in 1024 dimensions.',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog='For further questions see the README'
+    )
+
+    parser.add_argument(
+        'API_KEY',
+        help='Your Clarifai API KEY.'
+    )
+
+    args = parser.parse_args()
+
+    query_CLARIFAI(args.API_KEY)
