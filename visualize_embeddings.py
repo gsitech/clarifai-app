60b1845f90a92c4c8cf6213b57e9c2adf9332a95
Ubuntu 16.04 <shashanksundariyer@gmail.com>
2018-10-09 16:56:51 -0700

    Visualise data using T-SNE

--- visualize_embeddings.py
+++ visualize_embeddings.py
@@ -1,5 +1,3 @@
-"""Train the model"""
-
 import argparse
 import os
 import pathlib
@@ -17,9 +15,7 @@ from my_model import sup
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='experiments/base_model',
                     help="Experiment directory containing params.json")
-parser.add_argument('--data_dir', default='data/mnist',
-                    help="Directory containing the dataset")
-parser.add_argument('--sprite_filename', default='experiments/clarifai_sprite.png',
+parser.add_argument('--sprite_filename', default='experiments/css_sprites.png',
                     help="Sprite image for the projector")
 
 
@@ -32,28 +28,11 @@ if __name__ == '__main__':
     json_path = os.path.join(args.model_dir, 'params.json')
     assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
     params = Params(json_path)
-    '''
-    # Define the model
-    tf.logging.info("Creating the model...")
-    config = tf.estimator.RunConfig(tf_random_seed=230,
-                                    model_dir=args.model_dir,
-                                    save_summary_steps=params.save_summary_steps)
-    estimator = tf.estimator.Estimator(model_fn, params=params, config=config)
-
-
-    # EMBEDDINGS VISUALIZATION
-
+    
     # Compute embeddings on the test set
     tf.logging.info("Predicting")
-    predictions = estimator.predict(lambda: test_input_fn())
-    '''
     embeddings, _ = sup(params, 'test')
-    '''
-    # TODO (@omoindrot): remove the hard-coded 10000
-    embeddings = np.zeros((10000, params.embedding_size))
-    for i, p in enumerate(predictions.tolist()):
-        embeddings[i] = p['embeddings']
-    '''
+    
     tf.logging.info("Embeddings shape: {}".format(embeddings.shape))
 
     # Visualize test embeddings
@@ -69,27 +48,9 @@ if __name__ == '__main__':
     
     # Specify where you find the sprite (we will create this later)
     # Copy the embedding sprite image to the eval directory
-    
-    #shutil.copy2(args.sprite_filename, eval_dir)
-    #embedding.sprite.image_path = pathlib.Path(args.sprite_filename).name
-    #embedding.sprite.single_image_dim.extend([50, 50])
-    
-    '''
-    with tf.Session() as sess:
-        # TODO (@omoindrot): remove the hard-coded 10000
-        # Obtain the test labels
-        
-        dataset = mnist_dataset.test(args.data_dir)
-        dataset = dataset.map(lambda img, lab: lab)
-        dataset = dataset.batch(10000)
-        labels_tensor = dataset.make_one_shot_iterator().get_next()
-        
-        _, labels = test_input_fn()
-        labels_tensor = tf.Variable(labels, name = 'Labels')
-        labels = sess.run(labels_tensor.initializer)
-        labels = sess.run(labels_tensor)
-
-    '''
+    shutil.copy2(args.sprite_filename, eval_dir)
+    embedding.sprite.image_path = pathlib.Path(args.sprite_filename).name
+    embedding.sprite.single_image_dim.extend([100, 100])
 
     # Specify where you find the metadata
     # Save the metadata file needed for Tensorboard projector
@@ -99,9 +60,9 @@ if __name__ == '__main__':
             f.write('{}\n'.format(c))
     embedding.metadata_path = metadata_filename
     
-    # Say that you want to visualise the embeddings
+    # Visualise the embeddings
     projector.visualize_embeddings(tf.summary.FileWriter(eval_dir), config)
-    #e_r = tf.Variable(initial_value = embedding_var, dtype=tf.float64, name = 'e_r')
+    
     with tf.Session() as ses:
         sav = tf.train.Saver([embedding_var])
         ses.run(embedding_var.initializer)
